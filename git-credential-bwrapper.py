fcb40b2e421e8d249a912990ff1f2a80d8b1bfaf
Waiss Azizian <waissfowl@gmail.com>
2020-07-14 18:50:39 +0200

    Add debug output and argument parser

--- git-credential-bwrapper.py
+++ git-credential-bwrapper.py
@@ -3,9 +3,26 @@
 import sys
 import subprocess
 import json
+import logging
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('operation', metavar='OP', type=str, help="git credential operation, only get is supported")
+parser.add_argument('--debug', action='store_true')
+args = parser.parse_args()
+
+if args.operation != 'get':
+    logging.debug("Wrong operation: " + args.operation)
+    exit(0)
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
 
 def check_bw_status():
-    process = subprocess.run(['bw', 'status'], stdout=subprocess.PIPE)
+    logging.debug("Checking Bitwarden status...")
+    process = subprocess.run(['bw', 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    logging.debug("Bitwarden output:\n\tstdout: {}\n\tstderr: {}".format(process.stdout.decode('utf-8'),
+                                                                     process.stderr.decode('utf-8')))
     try:
         bw_status = json.loads(process.stdout)
         if bw_status['status'] != 'unlocked':
@@ -15,9 +32,6 @@ def check_bw_status():
 
 OUT_ATTRIBUTES = ['username', 'password']
 
-if sys.argv[1] != 'get':
-    exit(0)
-
 info = {}
 for line in sys.stdin:
     line = line.rstrip()
@@ -26,13 +40,22 @@ for line in sys.stdin:
     splitted_line = line.split('=', 1)
     info[splitted_line[0]] = splitted_line[1]
 
+logging.debug("Received information from git:\n\t{}".format(info))
+
 #TODO: check protocol
 
 base_domain = info['host'].split('.')[-2]
+command = ['bw', 'get', 'item', base_domain]
 
-process = subprocess.run(['bw', 'get', 'item', base_domain],
+logging.debug("Bitwarden query: {}".format(' '.join(command)))
+
+process = subprocess.run(command,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
+
+logging.debug("Bitwarden output:\n\tstdout: {}\n\tstderr: {}".format(process.stdout.decode('utf-8'),
+                                                                 process.stderr.decode('utf-8')))
+
 #TODO: check stderr
 
 try:
